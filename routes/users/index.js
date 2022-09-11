04bfd9a936811de86433b99004ee0021ecb64b98
oleksandr hrubyi <oleksandr.hrubyi@wpt.dev>
2022-09-11 22:09:08 +0300

    hw5

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { registration, login, logout, updateAvatar, refresh, getFavorites } = require("../../controllers/users");
+const { registration, login, logout, updateAvatar, refresh } = require("../../controllers/users");
 const guard = require("../../helper/guard");
 const uploadAvatar = require('../../helper/upload-avatar')
 
@@ -8,14 +8,10 @@ router.post("/signup", registration);
 router.post("/login", login);
 router.get("/current", guard, refresh);
 router.post("/logout", guard, logout);
-router.param("/:filmId/favorite", guard, getFavorites)
 router.patch(
     "/avatars",
     guard,
     uploadAvatar.single('avatar'),
     updateAvatar)
-
-// router.get('/verify/:token', verify)
-// router.post('/verify', repeatEmailVerify)
 
 module.exports = router;
